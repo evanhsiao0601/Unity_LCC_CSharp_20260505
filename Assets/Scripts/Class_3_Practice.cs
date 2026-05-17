102ccd4c6114ae247029c1cdb56d408e92952671
evanhsiao0601 <evanhsiao0601@gmail.com>
2026-05-17 15:34:10 +0800

    Update 練習3和練習4

---
+++ Assets/Scripts/Class_3_Practice.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+public class Class_3_Practice : MonoBehaviour
+{
+    private void Awake()
+    {
+        Debug.Log(6 / 2 *(1 + 2));
+    }
+
+}
---
+++ Assets/Scripts/Class_4_Practice.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+public class Class_4_Practice : MonoBehaviour
+{
+    private void Awake()
+    {
+        //呼叫有傳回的方法
+        bool BoolTrue = BoolMathsTrue();
+        Debug.Log($"<color=#f39>結果是：{BoolTrue}</color>");
+
+        bool boolfalse = BoolMathsfalse();
+        Debug.Log($"<color=#f39>結果是：{boolfalse}</color>");
+
+    }
+
+    //先定義出兩個數字變數
+    private int a = 2, b = 3; 
+    //再利用這兩個數字變數進行布林運算的比大小
+    private bool BoolMathsTrue()
+    {
+        return (a < b);
+    }
+    private bool BoolMathsfalse() 
+    {
+        return (a > b);
+    }
+
+}
