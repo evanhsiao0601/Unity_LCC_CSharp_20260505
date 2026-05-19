ba0cf643fd036c45defe2cf6f0fe722a83afc7cd
evanhsiao0601 <evanhsiao0601@gmail.com>
2026-05-19 14:36:58 +0800

    Update 練習3和4

--- Assets/Scripts/Class_3_Practice.cs
+++ Assets/Scripts/Class_3_Practice.cs
@@ -4,6 +4,9 @@ public class Class_3_Practice : MonoBehaviour
 {
     private void Awake()
     {
+        int answer = 6 / 2 * (1 + 2);
+        Debug.Log($"<color=#33>答案是{answer}</color>");
+
         Debug.Log(6 / 2 *(1 + 2));
     }
 
--- Assets/Scripts/Class_4_Practice.cs
+++ Assets/Scripts/Class_4_Practice.cs
@@ -11,6 +11,10 @@ public class Class_4_Practice : MonoBehaviour
         bool boolfalse = BoolMathsfalse();
         Debug.Log($"<color=#f39>結果是：{boolfalse}</color>");
 
+        //不用先定義 直接把方法放入 
+        Debug.Log($"<color=#f93>{ReturnTrue()}</color>");
+        Debug.Log($"<color=#f93>{ReturnFasle()}</color>");
+
     }
 
     //先定義出兩個數字變數
@@ -23,6 +27,17 @@ public class Class_4_Practice : MonoBehaviour
     private bool BoolMathsfalse() 
     {
         return (a > b);
+    }
+
+    //直接傳回true或fasle
+    private bool ReturnTrue() 
+    {
+        return true;
+    }
+
+    private bool ReturnFasle() 
+    {
+        return false;
     }
 
 }
