160893396b9635dc275b1eb29e13804482f7f59d
evanhsiao0601 <evanhsiao0601@gmail.com>
2026-05-21 20:56:36 +0800

    Update 迴圈_陣列

--- Assets/Scripts/Class_6_1_Iteration.cs
+++ Assets/Scripts/Class_6_1_Iteration.cs
@@ -9,15 +9,16 @@ public class Class_6_1_Iteration : MonoBehaviour
 {
     private void Awake()
     {
-        
-    //迴圈需要在一次性事件使用 Awake Start
-    //1. while迴圈：當布林值為true時會持續執行
-    //語法：(布林值) {程式碼區塊}
-    //無限迴圈：布林值一直是true
+
+        #region 迴圈 while do for
+        //迴圈需要在一次性事件使用 Awake Start
+        //1. while迴圈：當布林值為true時會持續執行
+        //語法：(布林值) {程式碼區塊}
+        //無限迴圈：布林值一直是true
 
         //定義區域變數a是0
         //int a = 0;
-         int a = 5; 
+        int a = 5;
         //當a < 5就執行{}內的程式碼 當a等於5就不會執行
         while (a < 5)
         {
@@ -31,7 +32,7 @@ public class Class_6_1_Iteration : MonoBehaviour
         //語法：do{程式碼區塊} while(布林值)
         //do跟while的差異在 do在前面的話 一定會先執行一遍 即使b等於5 也會執行一次
         //如果只有while在前面 會先判斷是不是等於5 如果是就不會執行
-        
+
         //定義區域變數b是0
         //int b = 0; 
         int b = 5; //一定會先執行一次 所以Log會顯示5
@@ -49,7 +50,22 @@ public class Class_6_1_Iteration : MonoBehaviour
         {
             Debug.Log($"<color=#f79>for迴圈c={c}</color>");
         }
+        #endregion
 
+        #region 迴圈 foreach
+        //3. foreach 迴圈
+        //語法：foreach (資料類型 資料名稱 in 資料結構) {程式碼區塊}
+        //資料結構：陣列 清單 字串等
+        //字串用來存放char陣列
+        string dialogue = "嗨 你好";
+        //foreach迴圈會抓出dialogue的每一個字並存放在區域變數的text內 每個字元個別顯示出來
+        //var 無類型
+        foreach (var text in dialogue)
+        {
+            Debug.Log($"<color=#f63>{text}</color>");
+        }
+
+        #endregion
     }
 
 }
---
+++ Assets/Scripts/Class_6_2_Array.cs
@@ -0,0 +1,80 @@
+using UnityEngine;
+namespace Evan
+{
+    /// <summary>
+    /// 陣列(Array)
+    /// </summary>
+    public class Class_6_2_Array : MonoBehaviour
+    {
+        //2*3*2 兩頁 一排三個 有兩排
+
+        //當碰到需要輸入大量且同類型的資料
+        //不使用陣列的寫法：
+        public string card1 = "皮卡丘", card2 = "小火龍", card3 = "卡比獸";
+
+        //使用陣列的寫法：
+        //一維：由上而下一排 二維：由上而下二排 三維：除了多排外增加第二頁
+        //陣列：用來儲存多筆相同類型的資料
+        //第一種：不指定值 透過Unity面板輸入
+        public string[] cards;
+        //第二種：直接定義陣列數量
+        public string[] deck1 = new string[5];
+        //第三種：直接定義陣列的值
+        public string[] deck2 = { "急凍鳥", "火焰鳥", "閃電鳥" };
+
+        //二維陣列的寫法：             第一排第一個   第二個  第二排第一個  第二個
+        public string[,] inventory = { { "紅藥水", "藍藥水" },{ "炸彈", "金幣" } };
+
+        //三維陣列的寫法：
+        public string[,,] shop =
+        {
+            //第一頁
+            { { "小刀", "美工刀" },{ "武士刀", "屠龍刀" }  },
+            //第二頁
+            { { "精靈球", "高級球" },{ "大師球", "巢穴球" }  }
+        };
+
+
+        private void Start()
+        {
+            #region 一維陣列
+            //存取陣列(Set Get)
+            //Get 取得陣列資料
+            //語法：陣列名稱[編號]
+            Debug.Log($"<color=#f32>牌組的第三張卡牌是{cards[2]}</color>");
+            //超出陣列會導致遊戲錯誤和閃退
+
+            //Set 設定陣列資料
+            //語法：陣列名稱[編號] 指定值
+            //將閃電鳥換成傑尼龜
+            deck2[2] = "傑尼龜";
+            Debug.Log($"<color=#f32>牌組的第三張卡牌是{deck2[2]}</color>");
+            #endregion
+
+            #region 二維陣列
+            //存取二維陣列
+            Debug.Log($"<color=#3f3>編號[0, 1]的道具是{inventory[0, 1]}</color>");
+            //替換[1, 1]的道具
+            inventory[1, 1] = "復活藥";
+            Debug.Log($"<color=#3f3>編號[1, 1]的道具是{inventory[1, 1]}</color>");
+            #endregion
+
+            #region 三維陣列
+            //存取三維陣列
+            //取得屠龍刀
+            Debug.Log($"<color=#3f3>第一頁第二排第二個的道具是{shop[0, 1, 1]}</color>");
+
+            shop[1, 0, 1] = "超級球";
+            Debug.Log($"<color=#3f3>第二頁第一排第二個的道具是{shop[1, 0, 1]}</color>");
+            #endregion
+
+            //獲得陣列的長度和維度
+            //一維到多維陣列長度(有幾個值)的寫法：陣列名稱.Length
+            Debug.Log($"<color=#3f3>牌組二的長度是{deck2.Length}</color>");
+            //一維到多維陣列維度的寫法：陣列名稱.Rank
+            Debug.Log($"<color=#3f3>牌組二的維度是{deck2.Rank}</color>");
+        }
+
+    }
+
+}
