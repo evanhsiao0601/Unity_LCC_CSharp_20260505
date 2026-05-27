1e8e3ef184fef422d9addae7dd98bbc6189eed99
evanhsiao0601 <evanhsiao0601@gmail.com>
2026-05-27 16:02:03 +0800

    Update 不規則和不規則多維陣列

--- Assets/Scripts/Class_6_2_Array.cs
+++ Assets/Scripts/Class_6_2_Array.cs
@@ -6,8 +6,8 @@ namespace Evan
     /// </summary>
     public class Class_6_2_Array : MonoBehaviour
     {
-        //2*3*2 第幾頁數 第幾排 第幾個 第一個頁/排/個都是從0開始算 例如0123是四排
 
+        #region 一到三維陣列
         //當碰到需要輸入大量且同類型的資料
         //不使用陣列的寫法：
         public string card1 = "皮卡丘", card2 = "小火龍", card3 = "卡比獸";
@@ -23,7 +23,7 @@ namespace Evan
         public string[] deck2 = { "急凍鳥", "火焰鳥", "閃電鳥" };
 
         //二維陣列的寫法：             第一排第一個   第二個  第二排第一個  第二個
-        public string[,] inventory = { { "紅藥水", "藍藥水" },{ "炸彈", "金幣" } };
+        public string[,] inventory = { { "紅藥水", "藍藥水" }, { "炸彈", "金幣" } };
 
         //三維陣列的寫法：
         public string[,,] shop =
@@ -33,10 +33,19 @@ namespace Evan
             //第二頁
             { { "精靈球", "高級球" },{ "大師球", "巢穴球" }  }
         };
+        #endregion
 
+        #region //不規則和不規則多維陣列
+        //不規則陣列用[][] new int [2]代表兩排 []個數為空 代表之後可以不規則定義
+        private int[][] numbers = new int[2][];
+        //不規則多維陣列用[][,] [2]是頁數
+        private int[][,] numbersb = new int[2][,]; 
+        #endregion
 
         private void Start()
         {
+
+            //cards [0, 0, 0]內的數字代表[頁, 排, 第幾個] 從0開始起算第一頁/排/個
             #region 一維陣列
             //存取陣列(Set Get)
             //Get 取得陣列資料
@@ -68,11 +77,35 @@ namespace Evan
             Debug.Log($"<color=#3f3>第二頁第一排第二個的道具是{shop[1, 0, 1]}</color>");
             #endregion
 
+            #region 陣列長度與維度
             //獲得陣列的長度和維度
             //一維到多維陣列長度(有幾個值)的寫法：陣列名稱.Length
             Debug.Log($"<color=#3f3>牌組二的長度是{deck2.Length}</color>");
             //一維到多維陣列維度的寫法：陣列名稱.Rank
             Debug.Log($"<color=#3f3>牌組二的維度是{deck2.Rank}</color>");
+            #endregion
+
+            #region //不規則和不規則多維陣列
+            //1. 不規則陣列
+            //numbers[第幾排] new int[]定義個數和其代表的數值 { 1, 3, 5 }三個個數 數值為135
+            numbers[0] = new int[] { 1, 3, 5 };
+            numbers[1] = new int[] { 9, 8 };
+
+            //numbers[0][1] 第一排第二個
+            Debug.Log($"<color=#3ff>不規則陣列數字3：{numbers[0][1]}</color>");
+            //numbers[1][0] 第二排第一個
+            Debug.Log($"<color=#3ff>不規則陣列數字9：{numbers[1][0]}</color>");
+
+            //把第二排第二個從8改成6
+            numbers[1][1] = 6;
+            Debug.Log($"<color=#3ff>不規則陣列數字9：{numbers[1][0]}</color>");
+
+            //2. 不規則多維陣列
+            numbersb[0] = new int[,] { { 1, 1 }, { 1, 1 } };
+            numbersb[1] = new int[,] { { 2, 2, 2 }, { 2, 2, 2 } };
+            Debug.Log($"<color=#3ff>不規則多維陣列第二頁的[0, 0]：{numbersb[1][0, 0]}</color>"); 
+            #endregion
+
         }
 
     }
---
+++ Assets/Scripts/Class_7_Property.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+namespace Evan
+{
+    /// <summary>
+    /// 屬性(Property)
+    /// </summary>
+    public class Class_7_Property : MonoBehaviour
+{
+        //公開的變數：可以讓外部讀取和寫入
+        public float moveSpeed = 3.5f;
+        //私人的變數：不允許讓外部讀取和寫入
+        public float turnSpeed = 1.7f;
+
+    }
+
+}
