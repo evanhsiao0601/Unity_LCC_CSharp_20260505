08f10f29d332b8afa60034b4b7be4261acbbd14c
evanhsiao0601 <evanhsiao0601@gmail.com>
2026-06-24 15:09:36 +0800

    Update 課程15_介面

---
+++ Assets/Scripts/Class_15_Interface.cs
@@ -0,0 +1,77 @@
+﻿using UnityEngine;
+namespace Evan
+{
+    /// <summary>
+    /// 介面 Interface
+    /// </summary>
+    public class Class_15_Interface : MonoBehaviour
+    {
+        //如果取得隨機物品 按下數字鍵1使用 會根據物品的種類自動執行相對的方式
+        //模擬物品欄的第一格
+        public object inventory_1;
+        private void Awake()
+        {
+            //創造隨機變數 範圍0到3以內 (但Unity不會出現最大值 即3) 
+            int random = Random.Range(0, 3);
+            LogWithColor.LogWithColors($"隨機：{random}", "#f33");
+
+            //根據不同的隨機數字 在道具欄第一格實體化不同的物件
+            if (random == 0) inventory_1 = new Prop();
+            else if (random == 1) inventory_1 = new Equipment();
+            else if (random == 2) inventory_1 = new Map();
+
+        }
+        private void Update()
+        {
+            //當按下數字鍵1
+            if(Input.GetKeyDown(KeyCode.Alpha1))
+            {
+                //如果隨機的道具是Prop 就直接建立一個變數命名為prop
+                if (inventory_1 is Prop prop)
+                {
+                    //就可以直接使用Prop類別底下的Use方法 如果沒有把inventory_1轉型為Prop就無法使用
+                    prop.Use();
+                }
+                else if (inventory_1 is Equipment equip)
+                {
+                    equip.Use();
+                }
+                else if (inventory_1 is Map map)
+                {
+                    map.Use();
+                }
+                //第二種轉型寫法 if (inventory_1 is Prop prop)((Prop)inventory_1).Use();
+                
+                //以上兩種if式道具欄寫法不建議使用 因數量一多 容易造成混亂
+                //建議方法在InterfaceObject腳本
+            }
+        }
+    }
+
+    public class Prop
+    {
+        public void Use()
+        {
+            LogWithColor.LogWithColors("使用道具 恢復體力", "#f96");
+        }
+
+    }
+    
+    public class Equipment
+    {
+        public void Use()
+        {
+            LogWithColor.LogWithColors("使用裝備 裝到對應位置", "#f9f6");
+        }
+
+    }
+
+    public class Map
+    {
+        public void Use()
+        {
+            LogWithColor.LogWithColors("使用地圖 開啟地圖功能", "#96f");
+        }
+    }
+
+}
---
+++ Assets/Scripts/Class_15_InterfaceObject.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+namespace Evan
+{
+    /// <summary>
+    /// 介面物件：實作介面的物件
+    /// </summary>
+    public class Class_15_InterfaceObject
+    {
+
+    }
+
+    //介面的使用步驟
+    //1. 定義介面和介面成員
+    //2. 類別實作介面
+    //3. 實作介面成員
+
+    //步驟1
+    //建立帶有使用功能的介面 遊戲內可被使用的物品都會繼承這個父類別
+    public interface IUse //命名規則：介面類型都會在變數名稱前加上大寫i　
+    {
+        //不用宣告方法主體 不用寫大括號
+        public void Use();
+    }
+
+    //步驟2
+    //C#只能繼承一個父類別 但可以多重實作介面 (不同物品可以繼承同一個父類別)
+    public class Weapon : IUse 
+    {
+        public void Use()
+        {
+            Debug.Log($"<color=#fa3>使用武器，施展武器技能<color/>");
+        }
+    }
+    public class Potions : IUse
+    {
+        public void Use()
+        {
+            Debug.Log($"<color=#fa3>使用藥水，恢復魔力<color/>");
+        }
+    }
+    public class Chest : IUse
+    {
+        public void Use()
+        {
+            Debug.Log($"<color=#fa3>使用寶箱，獲得隨機道具<color/>");
+        }
+    }
+}
