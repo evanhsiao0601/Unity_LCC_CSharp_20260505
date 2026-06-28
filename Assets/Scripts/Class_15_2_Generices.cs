bacce3a160e9e48c30ed705095b963a1fc383915
evanhsiao0601 <evanhsiao0601@gmail.com>
2026-06-28 15:46:02 +0800

    Update 泛型清單 堆疊 佇列

--- Assets/Scripts/Class_15_2_Generices.cs
+++ Assets/Scripts/Class_15_2_Generices.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using UnityEditor.Experimental.GraphView;
 using UnityEngine;
+using static Evan.Class_15_2_Generices;
 namespace Evan
 {
     public class Class_15_2_Generices : MonoBehaviour
@@ -39,10 +41,36 @@ namespace Evan
             //在建立物件時 <>括號內決定泛型的型別
             var player1 = new DataPlayer<int>();
             player1.data = 99;
-            player1.LogData(123); 
+            player1.LogData(123);
             #endregion
 
         }
+        
+        private void Start()
+        {
+            #region 使用複數的泛型
+            var player = new player();
+            var enemy = new Enemy();
+            var attackEvent = new AttackEvent<player, Enemy>();
+            attackEvent.Attack(player, enemy);
+            #endregion
+
+            #region 泛型介面
+            var hp = new Hp();
+            var attack = new Attack();
+            hp.Increase(10.5f);
+            attack.Increase(50);
+            hp.Increase(3.5f);
+            #endregion
+
+            #region 泛型約束
+            var checker = new CheckValue<Hp, float>();
+            //hp出自泛型介面裡的var hp = new Hp();
+            checker.Check(hp); 
+            #endregion
+
+        } 
+        
 
         #region 數字對調
         public void SwapNumber(ref int a, ref int b)
@@ -98,7 +126,104 @@ namespace Evan
                 LogWithColor.LogWithColors(data, "#3ff");
             }
 
-        } 
+        }
         #endregion
 
+        #region 使用複數泛型的類別
+        #region 說明
+        //使用複數的泛型建立一個玩家和敵人互相攻擊的方法
+
+        //將玩家和敵人放在同一個方法裡面可以精簡程式碼
+        //而不需要拆開寫成兩種方法如 玩家攻擊敵人PlayerAttackEnemy();和敵人攻擊玩家EnemyAttackPlayer();
+
+        //使用泛型可以把類別抽象化 而不用重複寫各種角色攻擊對方的方法 如以下
+        // public class AttackPlayerEnemy 玩家攻擊敵人
+        //{
+        //    public void Attack(Player player, Enemy enemy)
+        //    {
+
+        //    }
+        //}
+        //public class AttackEnemyPlayer 敵人攻擊玩家
+        //{
+        //    public void Attack(Enemy enemy, Player player)
+        //    {
+
+        //    }
+        //}
+        // public void Attack(T attacker, U defender) 泛型的T和U就可以代表任何攻擊者和防禦者
+        // U位置放任何防禦者的類別如Enemy類別 defender是變數名稱 意思是當要執行攻擊或扣血等行為時
+        // 就使用例如defender.HP -= 10; defender就可以直接代表任何角色的類別
+        // 建立物件時再決定：AttackEvent<Player, Enemy> 或著 AttackEvent<Enemy, Player> 也可以AttackEvent<NPC, Boss> 
+        #endregion
+        public class player { }
+        public class Enemy { }
+        //習慣的泛型代號 T, U, V
+        public class AttackEvent<T, U>
+        {
+            public void Attack(T attacker, U defender)
+            {
+                LogWithColor.LogWithColors($"{attacker}攻擊{defender}", "#f3f");
+            }
+        }
+        #endregion
+
+        #region 泛型介面
+        public interface IStat<T>
+        {
+            //該狀態的值 T是暫代型別 之後實作再帶入如int float等數字型別
+            //value 變數名稱
+            //{ get; set; } 可以被讀取（get）也可以被修改（set）
+            public T value { get; set; }
+            //如果在 set { ... } 裡面有 value  那是 C# 保留的特殊變數 其他地方的 value 則只是一般名稱 你可以自由命名
+            //增加該狀態數值 increase方法 T是暫代型別 amount變數名稱
+            public void Increase(T amount);
+        }
+
+        public class Attack : IStat<int>
+        {
+            //定義T為int value是變數名稱
+            //介面（interface）不能宣告欄位（Field）不能用public int value;  因此要使用{ get; set; }
+            //C# 的開發習慣也會優先使用屬性而非公開欄位 因為保留未來加入驗證 通知或其他邏輯的彈性
+            public int value { get; set; }
+            //沿用Istate介面的方法 將T帶入int
+            public void Increase(int amount)
+            {
+                //int的值 加上之後在Start裡面帶入的值到amount中
+                //int的值可指定預設值 目前無
+                //+=代表 value值加上amount值產生的結果
+                value += amount;
+                LogWithColor.LogWithColors($"攻擊力：{value}", "#f3f");
+            }
+        }     
+
+        public class Hp : IStat<float>
+        {
+            public float value { get; set; }
+            public void Increase(float amount)
+            {
+                value += amount;
+                LogWithColor.LogWithColors($"血量：{value}", "#f3f");
+            }
+
+        }
+        #endregion
+
+        #region 泛型的約束
+        //限制 T 必須是一個有實作 IStat<U> 介面的型別
+        //當你使用 CheckValue<某個型別>時 編譯器會檢查那個型別是否符合這個限制
+        //如果符合就允許，不符合就直接編譯錯誤
+
+        //CheckValue<T, U>裡面的T和U 代表public class Hp : IStat<float>裡面的Hp和float
+        //現在要檢查的是型別 相對位置是第二個即U(float) 因此where T : IStat<U> 括號內要填U
+        public class CheckValue<T, U> where T : IStat<U>
+        {
+            public void Check(T stat)
+            {
+                LogWithColor.LogWithColors($"狀態的值：{stat.value}", "#f3d3");
+            }
+        } 
+        #endregion
     }
+
+}
---
+++ Assets/Scripts/Class_16_DataStruct.cs
@@ -0,0 +1,154 @@
+﻿using NUnit.Framework;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using UnityEngine;
+using static Evan.Class_15_2_Generices;
+using static UnityEditor.Progress;
+namespace Evan
+{
+    /// <summary>
+    /// 資料結構 (DataStruct)
+    /// </summary>
+    public class Class_16_DataStruct : MonoBehaviour
+    {
+        #region 泛型清單
+        //一般陣列
+        public int[] attacks = { 10, 15, 7 };
+        public float[] defens = { 1.5f, 7.5f, 0.3f };
+
+        //清單：泛型集合
+        //修飾詞 關鍵字List <型別> 清單名稱
+        public List<int> speeds = new List<int>() { 3, 9, 7 };
+        public List<string> props = new List<string>() { "藥水", "地圖" };
+        public List<float> playerDefens;
+        #endregion
+        private void Awake()
+        {
+            #region 泛型清單
+            //清單存取與陣列相同 但陣列初始化後就無法增減長度
+            //顯示第三筆資料 
+            LogWithColor.LogWithColors($"第三筆{speeds[2]}", "#3f3");
+            //數值改成10
+            speeds[0] = 10;
+            LogWithColor.LogWithColors($"第三筆{speeds[0]}", "#3f3");
+            //添加一筆道具
+            props.Add("寶劍");
+            //刪除第一筆道具
+            props.RemoveAt(0);
+            //添加頭盔到編號1上
+            props.Insert(1, "頭盔");
+
+            //foreach 依序取得資料 取得一筆執行一次 var 不指定型別 prop單筆資料從props清單內各筆拿出
+            foreach (var prop in props)
+            {
+                LogWithColor.LogWithColors($"道具{prop}", "#3f3");
+            }
+
+            //將建構子帶入陣列
+            //建立一個新的List<float> 並把defens裡面的所有資料複製到新的清單中
+            playerDefens = new List<float>(defens);
+            //排序 由小到大
+            playerDefens.Sort();
+            //反排序 由大到小
+            playerDefens.Reverse();
+
+            //foreach 直接取出每一個元素
+            foreach (var item in playerDefens)
+            {
+                LogWithColor.LogWithColors($"資料{item}", "#f93");
+            }
+
+            //Count 查詢數量的關鍵字 指清單內的資料 根據Add或Remove改變
+            LogWithColor.LogWithColors($"數量{props.Count}", "#77f");
+            //Capacity 指清單內的容量 系統自動分配 預設為4 並以倍數成長(C#各版本不同)
+            LogWithColor.LogWithColors($"容量{props.Capacity}", "#77f");
+
+            List<int> numbers = new List<int>();
+
+            //for 自己控制次數或索引
+            //當i等於0 小於20時 從0開始 不斷遞增 每次加1 直到19 總共執行20次
+            //i++ 增加1
+            for (int i = 0; i < 20; i++)
+            {
+                //每次都會把i加進numbers清單內 會加19次 所以從0開始 0+1=1 1+1=2 以此類推19次
+                numbers.Add(i);
+                LogWithColor.LogWithColors($"數量{numbers.Count}", "#77f");
+                LogWithColor.LogWithColors($"容量{numbers.Capacity}", "#77f");
+            }
+            #endregion
+
+            #region 泛型堆疊
+            //關鍵字Stack
+            //適合用在遊戲選單 或是行為undo上面 永遠都是最後做的先取消
+
+            Stack<string> enemys = new Stack<string>();
+            //Push是Stack專用的方法 把一個元素放到 Stack 的最上面
+            enemys.Push("史萊姆");
+            //Stack 是最後放進去的東西 要最先拿出來 新的資料一定放在最上面 
+            //所以哥布林在Debug.Log裡面會先行顯示
+            enemys.Push("哥布林");
+            //執行下面LogStack方法
+            LogStack<string>(enemys);
+
+            //拿資料並且不移除 用Peek
+            var peek = enemys.Peek();
+            LogStack<string>(enemys);
+
+            //拿資料移除 用Pop
+            var pop = enemys.Pop();
+            LogStack<string>(enemys);
+
+            //判斷是否包含某筆資料 用Contains
+            LogWithColor.LogWithColors($"{enemys.Contains("哥布林")}", "#36f");
+
+            //清除所有資料
+            enemys.Clear();
+            LogStack<string>(enemys);
+            #endregion
+
+            #region 泛型佇列
+            //關鍵字 Queue
+            //先進先出 先放進來的資料先被拿出來
+            Queue<string> player = new Queue<string>();
+            player.Enqueue("盜賊");
+            player.Enqueue("法師");
+            player.Enqueue("戰士");
+            LogQueue<string>(player);
+
+            //拿出來不刪除 用Peek
+            LogWithColor.LogWithColors(player.Peek(), "#f33");
+            LogQueue<string>(player);
+
+            //拿出來並刪除 用Dequeue 與pop相同
+            LogWithColor.LogWithColors(player.Dequeue(), "#f33");
+            LogQueue<string>(player);
+            #endregion
+
+        }
+
+        #region 泛型堆疊
+        private void LogStack<T>(Stack<T> stack)
+        {
+            //逐次取出在stack內的每筆資料
+            foreach (var item in stack)
+            {
+                LogWithColor.LogWithColors($"堆疊資料 {item}", "#f77");
+            }
+        }
+        #endregion
+
+
+        #region 泛型佇列
+        private void LogQueue<T>(Queue<T> queue)
+        {
+            {
+                foreach (var item in queue)
+                {
+                    LogWithColor.LogWithColors($"佇列資料 {item}", "#7f7");
+                }
+            }
+
+        } 
+        #endregion
+    }
+}
